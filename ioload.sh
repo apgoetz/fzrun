8afcc0fef05768057af29e983fb730258476396b
Andrew P Goetz <goetza@rita.cat.pdx.edu>
2012-12-15 02:25:08 -0800

    Added ioload load simulator script

---
+++ ioload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#  Helper script to generate a large I/O load and simulate a runaway
+ 
+MAX=10				# make MAX concurrent iops
+
+# create 128Mbyte files
+for i in $(seq $MAX) ; do
+
+    file=$(tempfile -d .)
+    files[$i]=$file
+    dd if=/dev/zero of=$file bs=4k count=64k &
+    pids[$i]=$!
+done
+
+# wait for children to complete
+for p in ${pids[@]} ; do
+    wait $p
+done
+
+# delete the tmp files
+for i in $(seq $MAX) ; do
+    rm ${files[$i]}
+done
