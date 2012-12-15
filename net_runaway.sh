604abc72287f6f4ccd8fca8a534c2baceef5c46e
Andrew P Goetz <goetza@rita.cat.pdx.edu>
2012-12-15 02:47:29 -0800

    Modified net_runaway.sh script to prevent it from passing its arguments to fzrun

--- net_runaway.sh
+++ net_runaway.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # helper script to check for runaways in netgroup
+
+# make sure netgroup supplied
 if [[ $# -lt 1 ]] ; then 
     echo 'must supply netgroup (at least)'
     exit
 fi
 
+group=$1
+
+# remove the group argument, since it shouldn't be passed to fzrun
+set -- "${@:1:}" "${@:2}"
+
 # netgrouplist fails silently if invalid netgroup provided. 
-if [[ -z $(netgrouplist $1) ]] ; then
+if [[ -z $(netgrouplist $group) ]] ; then
     echo "empty netgrouplist. Possible typo with '$1' ?" > /dev/stderr
     exit 255
 fi
 
-netgrouplist $1 \
+# parallelize over entire set of boxen in netgroup
+netgrouplist $group \
 | xargs -I '{}' -P 0 \
-ssh -o StrictHostKeyChecking=no  '{}' ./fzrun $@ 2>/dev/null 
+ssh -o StrictHostKeyChecking=no  '{}' ./fzrun/fzrun $@ 2>/dev/null 
